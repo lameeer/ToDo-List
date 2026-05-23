4f99638cd9157c72988869aa7514fc02cb032a85
govnocods <135868811+lameeer@users.noreply.github.com>
2026-05-23 10:23:54 +0300

    fix: update TaskWidget

--- widgets.py
+++ widgets.py
@@ -1,20 +1,23 @@
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QCheckBox, QLabel, QPushButton
 
 class TaskWidget(QWidget):
-    def __init__(self, title, list_widget, item, main_window):
+    def __init__(self, task_data, list_widget, item, main_window):
         super().__init__()
         self.list_widget = list_widget
         self.item = item
         self.main_window = main_window
+        
+        self.task_data = task_data 
 
         layout = QHBoxLayout()
         layout.setContentsMargins(10, 0, 10, 0)
 
         self.checkbox = QCheckBox()
         self.checkbox.setStyleSheet("padding: 0px;")
+        self.checkbox.setChecked(self.task_data.get("completed", False)) 
         self.checkbox.stateChanged.connect(self.toggle_completed)
         
-        self.lbl_title = QLabel(title)
+        self.lbl_title = QLabel(self.task_data["title"]) 
         self.lbl_title.setStyleSheet("color: black; font-weight: bold; background: transparent; padding: 0px;")
         
         self.btn_edit = QPushButton("✎")
@@ -34,16 +37,27 @@ class TaskWidget(QWidget):
         layout.addWidget(self.btn_delete)
 
         self.setLayout(layout)
+        
+        self.update_text_style()
 
-    def toggle_completed(self, state):
-        if state == 2:
+    def update_text_style(self):
+        if self.checkbox.isChecked():
             self.lbl_title.setStyleSheet("text-decoration: line-through; color: #7f8c8d; background: transparent; padding: 0px;")
         else:
             self.lbl_title.setStyleSheet("color: black; font-weight: bold; background: transparent; padding: 0px;")
 
+    def toggle_completed(self, state):
+        self.task_data["completed"] = self.checkbox.isChecked()
+        self.update_text_style()
+        self.main_window.save_all_tasks() 
+
     def delete_task(self):
         row = self.list_widget.row(self.item)
         self.list_widget.takeItem(row)
+        
+        if self.task_data in self.main_window.tasks:
+            self.main_window.tasks.remove(self.task_data)
+        self.main_window.save_all_tasks()
 
     def edit_task(self):
-        self.main_window.open_edit(self.lbl_title)
+        self.main_window.open_edit(self)
